d49e3a092e480910b6ff1465ea4e22aa4056e1ba
Breanna Burris <breanna1burris@gmail.com>
2024-11-13 23:40:47 -0500

    Create ProductList comp to render each ProductItem

---
+++ product-dashboard/src/ProductList.jsx
@@ -0,0 +1,21 @@
+// Task 3: Create the ProductList Component
+
+import React from "react";
+import ProductItem from "./ProductItem";
+
+const ProductList = ({ products }) => {
+  return (
+    <div>
+      <h2>Product List</h2>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <ProductItem product={product} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProductList;
